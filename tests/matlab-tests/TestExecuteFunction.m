f947a39a9b9390e7bb8a2cf7ba606362fe5b39cf
Kumar Pallav <kpallav@vdi-bglddp-112.dhcp.mathworks.com>
2024-10-18 15:02:43 +0530

    execute tests

--- tests/matlab-tests/TestExecuteFunction.m
+++ tests/matlab-tests/TestExecuteFunction.m
@@ -48,11 +48,12 @@ classdef TestExecuteFunction < matlab.unittest.TestCase
 
         function testVariableStringOutput(testCase)
             % Test execution of a code that generates a variable string output
-            code = "a='variable string'; sprintf('Text %f of type variable string', a)";
+            % code = "a='variable string'; sprintf('Text %f of type variable string', a)";
+            code = "A = 1/eps;sprintf('%0.5f',A)"
             kernelId = 'test_kernel_id';
             result = jupyter.execute(code, kernelId);
             disp("varstring");
-            disp(struct(result{1}.content));
+            % disp(struct(result{1}.content));
             testCase.verifyEqual(result{1}.type, 'execute_result', 'Expected execute_result type');
             testCase.verifyTrue(any(strcmp(result{1}.mimetype{1}, 'text/html')), 'Expected HTML output');
             testCase.verifyTrue(any(strcmp(result{1}.mimetype{2}, 'text/plain')), 'Expected HTML output');
